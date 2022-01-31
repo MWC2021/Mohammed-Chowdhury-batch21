c7fa5d3f0ad9bdafd3beae0843f0caef977cc338
studentqa2021 <sarowerhome@gmail.com>
2022-01-31 01:18:55 -0500

    first commit

--- src/main/java/com/generic/BaseLogin.java
+++ src/main/java/com/generic/BaseLogin.java
@@ -1,28 +1,25 @@
-package com.generic;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class BaseLogin {
-	
-	public void getLogin() {
-	//java>> key means selenium package name value = driver path
-		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
-		//selenium==> need driver path/location
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://automationpractice.com/index.php");
-		driver.manage().window().maximize();
-				
-		//System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-		//driver.get("https://www.google.com/");
-		//driver.get("https://www.amazon.com/");
-		//driver.get("https://www.facebook.com/");
-		//driver.get("http://automationpractice.com/index.php");
-		//driver.manage().window().maximize();
-		
-		
-	
-	}
-
-}
+  package com.generic;
+  
+  import org.openqa.selenium.WebDriver; 
+  import org.openqa.selenium.chrome.ChromeDriver;
+  
+  public class BaseLogin {
+	  
+	  public void getLogin() {
+		  
+  System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+  WebDriver driver = new ChromeDriver();
+  
+  //driver.get("https://www.google.com/");
+  driver.get("https://www.amazon.com/");
+  //driver.get("https://www.facebook.com/");
+  //driver.get("http://automationpractice.com/index.php");
+  //driver.manage().window().maximize();
+  
+  
+  
+  }
+  
+  
+  }
--- src/main/java/com/util/BaseConfig.java
+++ src/main/java/com/util/BaseConfig.java
@@ -7,24 +7,19 @@ import java.util.Properties;
 
 public class BaseConfig {
 	
-public String getValue(String value) throws Exception {
+	public String getValue( String value) throws Exception {
 		
 		String path="./config.properties";//location of config file
-		FileInputStream fis = new FileInputStream(path);// into stream 
-		//read file data
-		Properties pro = new Properties();
+		FileInputStream fis = new FileInputStream(path);//into stream
+		//read file data 
+		Properties pro = new Properties(); 
 		pro.load(fis);
-		
-		//return value
+		//return value 
 		return pro.get(value).toString();
-	}
-		
-		
-		
-		
 		
 		
 		
 	}
-
-
+		
+		
+}
